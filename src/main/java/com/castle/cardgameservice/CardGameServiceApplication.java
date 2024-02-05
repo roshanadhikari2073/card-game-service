db730f56224486072359ea0a41f9eef724856340
rosanlang <greenboyroshan@gmail.com>
2024-02-05 15:49:17 -0800

    Initialized the spring project

---
+++ src/main/java/com/castle/cardgameservice/CardGameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.castle.cardgameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGameServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castle/cardgameservice/CardGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castle.cardgameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
