b023d94081e2af81167859293a9b1498fa193724
roshanadhikari2073 <roshanadhikari2073@gmail.com>
2024-02-05 16:53:00 -0800

    Setting up swagger in the project

--- src/main/java/com/castle/cardgameservice/config/SwaggerConfig.java
+++ src/main/java/com/castle/cardgameservice/config/SwaggerConfig.java
@@ -1,4 +1,17 @@
 package com.castle.cardgameservice.config;
 
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("cardgames")
+                .pathsToMatch("/api/**")
+                .build();
+    }
 }
